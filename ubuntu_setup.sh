ca26821fef97c8ff9b80d364925903ec8e7ebf5b
Marcela Rosales <marcelarosalesj@gmail.com>
2019-07-01 21:31:45 -0500

    Add docker install and configuration
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59,6 +59,29 @@ config_python(){
     source $HOME/.bashrc
 }
 
+config_docker(){
+    echo "Configuring docker"
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    sudo gpasswd -a $USER docker
+    newgrp docker
+}
+
 print_usage(){
     echo "$package - setup my personal environment"
     echo " "
@@ -73,6 +96,7 @@ print_usage(){
     echo "--python                  python"
     echo "--bash                    bash"
     echo "--system                  system"
+    echo "--docker                  docker"
 
 }
 
@@ -122,6 +146,11 @@ while [ "$1" != "" ]; do
             shift
             echo "Config bash"
             config_bash
+            ;;
+        --docker)
+            shift
+            echo "Config docker"
+            config_docker
             ;;
         *)
             shift
